7228e2d36e28fa87a47000b5d7996b7210b5f1ea
Matteo Affatato <matteoaffatato@MacBook-Air-di-Matteo.local>
2023-12-14 17:52:35 +0100

    secondo commit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// ESERCIZIO 1
+//   Lavoriamo sul DOM. Creare 3 paragrafi in HTML e 3 bottoni. 
+
+// Il primo bottone dovrà nascondere i 3 paragrafi e farli ricomparire
+// il secondo bottone dovrà cambiare i colori dei paragrafi in maniera casuale
+// il terzo bottone dovrà rendere i paragrafi in grassetto e farli tornare come prima.
+// PS: Uniamo le conoscenze che abbiamo appreso finora, come sappiamo quando c'è qualcosa di casuale possiamo utilizzare il Math.random().
+
+
 const button1 = document.querySelector('#btnCustom1');
 const button2 = document.querySelector('#btnCustom2');
 const button3 = document.querySelector('#btnCustom3');
